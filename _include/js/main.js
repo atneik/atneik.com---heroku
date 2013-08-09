a03d3715ff2bec1d15e92bf248de063c4539d956
Aniket Handa <atneik@gmail.com>
2013-08-10 03:05:47 +0530

    min added

--- _include/js/main.js
+++ _include/js/main.js
@@ -161,37 +161,6 @@ BRUSHED.contactForm = function(){
 
 
 /* ==================================================
-   Twitter Feed
-================================================== */
-
-BRUSHED.tweetFeed = function(){
-	var valueTop = -64;
-	
-    $("#ticker").tweet({
-          username: "Bluxart",
-          page: 1,
-          avatar_size: 0,
-          count: 10,
-		  template: "{text}{time}",
-		  filter: function(t){ return ! /^@\w+/.test(t.tweet_raw_text); },
-          loading_text: "loading ..."
-	}).bind("loaded", function() {
-	  var ul = $(this).find(".tweet_list");
-	  var ticker = function() {
-		setTimeout(function() {
-			ul.find('li:first').animate( {marginTop: valueTop + 'px'}, 500, 'linear', function() {
-				$(this).detach().appendTo(ul).removeAttr('style');
-			});	
-		  ticker();
-		}, 5000);
-	  };
-	  ticker();
-	});
-	
-}
-
-
-/* ==================================================
    Menu Highlight
 ================================================== */
 
@@ -351,7 +320,7 @@ $(document).ready(function(){
 		nope: '_include/js/placeholder.js', 
 		complete : function() {
 				if (!Modernizr.placeholder) {
-						Placeholder.init({
+						placeholder.init({
 						live: true,
 						hideOnFocus: false,
 						className: "yourClass",
@@ -381,7 +350,6 @@ $(document).ready(function(){
 	BRUSHED.filter();
 	BRUSHED.fancyBox();
 	BRUSHED.contactForm();
-	BRUSHED.tweetFeed();
 	BRUSHED.scrollToTop();
 	BRUSHED.utils();
 	BRUSHED.accordion();
