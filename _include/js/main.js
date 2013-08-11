adb9ca6236637a4ab3a199986311b15260fa2b6d
Aniket Handa <atneik@gmail.com>
2013-08-12 04:12:56 +0530

    minor edits and removed min

--- _include/js/main.js
+++ _include/js/main.js
@@ -50,55 +50,6 @@ BRUSHED.nav = function(){
 }
 
 
-/* ==================================================
-   Filter Works
-================================================== */
-
-BRUSHED.filter = function (){
-	if($('#projects').length > 0){		
-		var $container = $('#projects');
-		
-		$container.isotope({
-		  // options
-		  animationEngine: 'best-available',
-		  itemSelector : '.item-thumbs',
-		  layoutMode : 'fitRows'
-		});
-	
-		
-		// filter items when filter link is clicked
-		var $optionSets = $('#options .option-set'),
-			$optionLinks = $optionSets.find('a');
-	
-		  $optionLinks.click(function(){
-			var $this = $(this);
-			// don't proceed if already selected
-			if ( $this.hasClass('selected') ) {
-			  return false;
-			}
-			var $optionSet = $this.parents('.option-set');
-			$optionSet.find('.selected').removeClass('selected');
-			$this.addClass('selected');
-	  
-			// make option object dynamically, i.e. { filter: '.my-filter-class' }
-			var options = {},
-				key = $optionSet.attr('data-option-key'),
-				value = $this.attr('data-option-value');
-			// parse 'false' as false boolean
-			value = value === 'false' ? false : value;
-			options[ key ] = value;
-			if ( key === 'layoutMode' && typeof changeLayoutMode === 'function' ) {
-			  // changes in layout modes need extra logic
-			  changeLayoutMode( $this, options )
-			} else {
-			  // otherwise, apply new options
-			  $container.isotope( options );
-			}
-			
-			return false;
-		});
-	}
-}
 
 
 /* ==================================================
@@ -320,7 +271,7 @@ $(document).ready(function(){
 		nope: '_include/js/placeholder.js', 
 		complete : function() {
 				if (!Modernizr.placeholder) {
-						placeholder.init({
+						Placeholder.init({
 						live: true,
 						hideOnFocus: false,
 						className: "yourClass",
